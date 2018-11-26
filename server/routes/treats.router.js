bf9768694c87a20691440dd3095ad24354987b5d
Travis Lang <tlang50@icloud.com>
2018-11-26 13:18:01 -0600

    add GET route server side

--- server/routes/treats.router.js
+++ server/routes/treats.router.js
@@ -3,9 +3,26 @@ const pool = require('../modules/pool');
 
 
 // GET /treats
+router.get( '/', ( req, res ) => {
+    let queryString = `SELECT * FROM "treats" ORDER BY "id" ASC;`;
+    pool.query( queryString )
+    .then( result => {
+        res.send( result.rows );
+    }).catch( err => {
+        console.log( 'error in GET query:', err );
+        res.sendStatus( 500 );
+    }); // end query
+}) // end GET route
 
 // POST /treats
-
+router.post( '/', ( req, res ) => {
+    let queryString = `INSERT INTO "treats" ("name", "description","pic")
+    VALUES ($1, $2, $3);`;
+    pool.query( queryString, [ req.body.name, req.body.description, req.body.link ] )
+    .then( result => {
+        res.sendStatus( 201 );
+    }).catch( )
+})
 // PUT /treats/<id>
 
 // DELETE /treats/<id>
