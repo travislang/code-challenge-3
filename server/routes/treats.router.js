9a79b1225341529fe2fdbd70bbc5e0aecc834625
Travis Lang <tlang50@icloud.com>
2018-11-26 13:31:06 -0600

    add delete route

--- server/routes/treats.router.js
+++ server/routes/treats.router.js
@@ -18,16 +18,28 @@ router.get( '/', ( req, res ) => {
 router.post( '/', ( req, res ) => {
     let queryString = `INSERT INTO "treats" ("name", "description","pic")
     VALUES ($1, $2, $3);`;
-    pool.query( queryString, [ req.body.name, req.body.description, req.body.link ] )
+    console.log(req.body);
+    pool.query( queryString, [ req.body.name, req.body.description, req.body.pic ] )
     .then( result => {
         res.sendStatus( 201 );
     }).catch( err => {
         console.log( 'error in POST query:', err );
         res.sendStatus( 500 );
-    }) // end query
-}) // end POST route
+    }); // end query
+}); // end POST route
 // PUT /treats/<id>
 
 // DELETE /treats/<id>
+router.delete( '/:id', ( req, res ) => {
+    let id = req.params.id;
+    let queryString = `DELETE FROM "treats" WHERE "id" = $1;`;
+    pool.query( queryString, [ id ] )
+    .then( result => {
+        res.sendStatus( 200 );
+    }).catch( err => {
+        console.log( 'there was an error in delete route:', err );
+        res.sendStatus( 500 );
+    });// end query
+});// end DELETE route
 
 module.exports = router;
